3ef2ed9ff4e8c9d8dd79060e70d8ea5d2bb34454
mtngz <70101199+mtngz@users.noreply.github.com>
2020-09-28 14:28:49 +0200

    add an item to the list in scripts.js
    using jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
-function newItem() {}
+function newItem() {
+  // jQuery code
+
+  // Add an item to the list
+  let li = $("<li></li>");
+  let inputValue = $("#input").val();
+  li.append(inputValue);
+
+  // add alert for empty inputValue
+  if (inputValue === "") {
+    alert("Please enter an action.");
+  } else {
+    $("#list").append(li);
+  }
+}
