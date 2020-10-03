e0424e44fc62aa0dcd9276b3137b3db8ca6be25c
mtngz <70101199+mtngz@users.noreply.github.com>
2020-10-03 13:27:40 +0200

    Update scripts.js
    create function first, then call the function

--- js/scripts.js
+++ js/scripts.js
@@ -19,19 +19,19 @@ function newItem() {
   }
 
   // add event double-click to crosds out item
-  li.on("dblclick", function crossOut() {
-    li.toggleClass("strike");
-  });
+  li.on("dblclick", crossOut());
 
   // add delete button
   let deleteButton = $("<deleteButton></deleteButton>");
   deleteButton.append(document.createTextNode("x"));
   li.append(deleteButton);
 
-  // add event click to delete List Item
-  deleteButton.on("click", function deleteListItem() {
+  function deleteListItem() {
     li.addClass("delete");
-  });
+  }
+
+  // add event click to delete List Item
+  deleteButton.on("click", deleteListItem());
 
   // reorder items
   $("#list").sortable(); // same as in javascript
