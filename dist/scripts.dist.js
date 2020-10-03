c7d5533936eb0b5e63fc42c42e74af6d1295c984
mtngz <70101199+mtngz@users.noreply.github.com>
2020-10-03 13:29:28 +0200

    Update scripts.dist.js

--- dist/scripts.dist.js
+++ dist/scripts.dist.js
@@ -3,14 +3,10 @@ function newItem() {
     t = $("#input").val();
   e.append(t),
     "" === t ? alert("Please enter an action.") : $("#list").append(e),
-    e.on("dblclick", function () {
-      e.toggleClass("strike");
-    });
-  let n = $("<deleteButton></deleteButton>");
-  n.append(document.createTextNode("x")),
-    e.append(n),
-    n.on("click", function () {
-      e.addClass("delete");
-    }),
+    e.on("dblclick", void e.toggleClass("strike"));
+  let l = $("<deleteButton></deleteButton>");
+  l.append(document.createTextNode("x")),
+    e.append(l),
+    l.on("click", void e.addClass("delete")),
     $("#list").sortable();
 }
